68b305916f54ba36c7f0cd45c44f66167ad47703
bao240899 <55775650+bao240899@users.noreply.github.com>
2019-09-26 15:11:07 +0700

    commit compire page

---
+++ compire.js
@@ -0,0 +1,322 @@
+var getValue = document.getElementsByName('product1')[0];
+var pr1 = '';
+getValue.addEventListener('input', function () {
+  pr1 = getValue.value;
+  var text = "";
+  switch (pr1) {
+    case 'CALVIN KEILN - LEATHER CK LOGO ZIP-AROUND CONTINENTAL WALLET':
+      {
+        text = `<div class="title-information-page"><a href="#" title="home">Home</a> / <a href="#" title="Gender">Women</a> / <a href="#" title="brand-name">Calvin Klein</a></div><br>
+        <div class="row">
+          <!-- Image of product-->
+          <div class="col-sm-12 col-lg-6 ">
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                  <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleControls" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleControls" data-slide-to="2"></li>
+                  </ol>
+                  <div class="carousel-inner ">
+                   <div class="carousel-item active">
+                      <img class="d-block w-100" src="http://localhost/eproject/CALVIN%20KLEIN%20-%20LOGO%20PLAQUE%20LARGE%20TRIFOLD%20WALLET/LOGO%20PLAQUE%20LARGE%20TRIFOLD%20WALLET%201.jfif" alt="First slide">
+                      <div class="carousel-caption d-none d-md-block">
+                      </div>
+                </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="../LOGO PLAQUE LARGE TRIFOLD WALLET 2.jfif" alt="Second slide">
+                      <div class="carousel-caption d-none d-md-block">
+                      </div>
+                    </div>
+              <div class="carousel-item">
+                      <img class="d-block w-100" src="LOGO PLAQUE LARGE TRIFOLD WALLET 3.jfif" alt="Third slide">
+                      <div class="carousel-caption d-none d-md-block">
+                      </div>
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                  </a>
+                </div>	
+          </div>
+          <div class="col-lg-6 col-sm-0">
+            <!-- Title of product-->
+            <div class="title-of-product">CALVIN KEILN - LOGO PLAQUE LARGE TRIFOLD WALLET</div>
+            <!--Price of product-->
+            <div class="Price">$78.00</div>
+            <div class="Property">
+              <p>COLOR : LIGHT SAND <br>
+                 SIZE : ONE SIZE <br>
+                 IN STOCK
+              </p>
+            </div>
+            <button class="add-to-bag">ADD TO BAG</button>
+            <!-- Detail of product-->
+            <div class="detail">
+              <div class="panel-heading">
+                <!-- Detail button-->
+                <br><h5 class="panel-title">
+                  <a data-toggle="collapse" href="#collapse">Detail</a>
+                </h5>
+              </div>
+              <!-- Collapse detail of product-->
+              <div id="collapse" class="panel-collapse collapse">
+                <p>designed with a prominent, polished logo plaque at the front, this spacious trifold wallet is made with soft faux leather, 14 card slots, a bill slot, a coin pocket and contemporary ck styling.</p>
+                <li>logo plaque large trifold wallet</li>
+                <li>faux leather</li>
+                <li>14 card slots, bill slot + coin pocket</li>
+                <li>metal plaque on the front</li>
+                <li>7.5" wide x 3.9" tall x 1.4" deep</li>
+                <li>presented in a box</li>
+                
+                <p>Style #: 37405099-064 <br>
+                Universal Style #: K60K605099</p>
+            </div>
+            <div class="panel-heading">
+              <h5 class="panel-title">
+                <a data-toggle="collapse" href="#collapse1">CARE & COMPOSITION</a>
+              </h5>
+            </div>
+            <div id="collapse1" class="panel-collapse collapse">
+              <p>
+                <li>faux leather</li>
+                <li>Imported</li>
+              </p>
+            </div>
+          </div> 
+        </div>
+      </div>`;
+        break;
+      }
+    case 'CALVIN KLEIN - LOGO PLAQUE LARGE TRIFOLD WALLET':
+      {
+        text =`<div class="title-information-page"><a href="#" title="home">Home</a> / <a href="#" title="Gender">Women</a> / <a href="#" title="brand-name">Calvin Klein</a></div><br>
+        <div class="row">
+          <!-- Image of product-->
+          <div class="col-sm-12 col-lg-6 ">
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                  <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleControls" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleControls" data-slide-to="2"></li>
+                  </ol>
+                  <div class="carousel-inner ">
+                   <div class="carousel-item active">
+                      <img class="d-block w-100" src="./LOGO PLAQUE LARGE TRIFOLD WALLET 1.jfif" alt="First slide">
+                      <div class="carousel-caption d-none d-md-block">
+                      </div>
+                </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="LOGO PLAQUE LARGE TRIFOLD WALLET 2.jfif" alt="Second slide">
+                      <div class="carousel-caption d-none d-md-block">
+                      </div>
+                    </div>
+              <div class="carousel-item">
+                      <img class="d-block w-100" src="LOGO PLAQUE LARGE TRIFOLD WALLET 3.jfif" alt="Third slide">
+                      <div class="carousel-caption d-none d-md-block">
+                      </div>
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                  </a>
+                </div>	
+          </div>
+          <div class="col-lg-6 col-sm-0">
+            <!-- Title of product-->
+            <div class="title-of-product">CALVIN KEILN - LOGO PLAQUE LARGE TRIFOLD WALLET</div>
+            <!--Price of product-->
+            <div class="Price">$78.00</div>
+            <div class="Property">
+              <p>COLOR : LIGHT SAND <br>
+                 SIZE : ONE SIZE <br>
+                 IN STOCK
+              </p>
+            </div>
+            <button class="add-to-bag">ADD TO BAG</button>
+            <!-- Detail of product-->
+            <div class="detail">
+              <div class="panel-heading">
+                <!-- Detail button-->
+                <br><h5 class="panel-title">
+                  <a data-toggle="collapse" href="#collapse">Detail</a>
+                </h5>
+              </div>
+              <!-- Collapse detail of product-->
+              <div id="collapse" class="panel-collapse collapse">
+                <p>designed with a prominent, polished logo plaque at the front, this spacious trifold wallet is made with soft faux leather, 14 card slots, a bill slot, a coin pocket and contemporary ck styling.</p>
+                <li>logo plaque large trifold wallet</li>
+                <li>faux leather</li>
+                <li>14 card slots, bill slot + coin pocket</li>
+                <li>metal plaque on the front</li>
+                <li>7.5" wide x 3.9" tall x 1.4" deep</li>
+                <li>presented in a box</li>
+                
+                <p>Style #: 37405099-064 <br>
+                Universal Style #: K60K605099</p>
+            </div>
+            <div class="panel-heading">
+              <h5 class="panel-title">
+                <a data-toggle="collapse" href="#collapse1">CARE & COMPOSITION</a>
+              </h5>
+            </div>
+            <div id="collapse1" class="panel-collapse collapse">
+              <p>
+                <li>faux leather</li>
+                <li>Imported</li>
+              </p>
+            </div>
+          </div> 
+        </div>
+      </div>`;
+        break;
+      }
+    case "nguyen": {
+      text = 'bao ngon zai';
+      break;
+    }
+    case "khoa": {
+      text = 'bao ngon zai';
+      break;
+    }
+    case "khanh": {
+      text = 'bao ngon zai';
+      break;
+    }
+    default: {
+      text = 'product 1';
+      break;
+    }
+  }
+  document.getElementById('pr1-compire').innerHTML = text;
+});
+
+
+
+var getValue1 = document.getElementsByName('product2')[0];
+var pr2 = '';
+getValue1.addEventListener('input', function () {
+  pr2 = getValue1.value;
+  var text2 = "";
+  switch (pr2) {
+    case "abc":
+      {
+        text2 = `<div class="title-information-page"><a href="#" title="home">Home</a> / <a href="#" title="Gender">Women</a> / <a href="#" title="brand-name">Calvin Klein</a></div><br>
+                <div class="row">
+                  <!-- Image of product-->
+                  <div class="col-sm-12 col-lg-6 ">
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                          <ol class="carousel-indicators">
+                            <li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExampleControls" data-slide-to="1"></li>
+                            <li data-target="#carouselExampleControls" data-slide-to="2"></li>
+                          </ol>
+                          <div class="carousel-inner ">
+                           <div class="carousel-item active">
+                              <img class="d-block w-100" src="http://localhost/eproject/CALVIN%20KLEIN%20-%20LOGO%20PLAQUE%20LARGE%20TRIFOLD%20WALLET/LOGO%20PLAQUE%20LARGE%20TRIFOLD%20WALLET%201.jfif" alt="First slide">
+                              <div class="carousel-caption d-none d-md-block">
+                              </div>
+                        </div>
+                            <div class="carousel-item">
+                              <img class="d-block w-100" src="LOGO PLAQUE LARGE TRIFOLD WALLET 2.jfif" alt="Second slide">
+                              <div class="carousel-caption d-none d-md-block">
+                              </div>
+                            </div>
+                      <div class="carousel-item">
+                              <img class="d-block w-100" src="LOGO PLAQUE LARGE TRIFOLD WALLET 3.jfif" alt="Third slide">
+                              <div class="carousel-caption d-none d-md-block">
+                              </div>
+                            </div>
+                          </div>
+                          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                          <span class="sr-only">Previous</span>
+                          </a>
+                          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                          <span class="sr-only">Next</span>
+                          </a>
+                        </div>	
+                  </div>
+                  <div class="col-lg-6 col-sm-0">
+                    <!-- Title of product-->
+                    <div class="title-of-product">CALVIN KEILN - LOGO PLAQUE LARGE TRIFOLD WALLET</div>
+                    <!--Price of product-->
+                    <div class="Price">$78.00</div>
+                    <div class="Property">
+                      <p>COLOR : LIGHT SAND <br>
+                         SIZE : ONE SIZE <br>
+                         IN STOCK
+                      </p>
+                    </div>
+                    <button class="add-to-bag">ADD TO BAG</button>
+                    <!-- Detail of product-->
+                    <div class="detail">
+                      <div class="panel-heading">
+                        <!-- Detail button-->
+                        <br><h5 class="panel-title">
+                          <a data-toggle="collapse" href="#collapse">Detail</a>
+                        </h5>
+                      </div>
+                      <!-- Collapse detail of product-->
+                      <div id="collapse" class="panel-collapse collapse">
+                        <p>designed with a prominent, polished logo plaque at the front, this spacious trifold wallet is made with soft faux leather, 14 card slots, a bill slot, a coin pocket and contemporary ck styling.</p>
+                        <li>logo plaque large trifold wallet</li>
+                        <li>faux leather</li>
+                        <li>14 card slots, bill slot + coin pocket</li>
+                        <li>metal plaque on the front</li>
+                        <li>7.5" wide x 3.9" tall x 1.4" deep</li>
+                        <li>presented in a box</li>
+                        
+                        <p>Style #: 37405099-064 <br>
+                        Universal Style #: K60K605099</p>
+                    </div>
+                    <div class="panel-heading">
+                      <h5 class="panel-title">
+                        <a data-toggle="collapse" href="#collapse1">CARE & COMPOSITION</a>
+                      </h5>
+                    </div>
+                    <div id="collapse1" class="panel-collapse collapse">
+                      <p>
+                        <li>faux leather</li>
+                        <li>Imported</li>
+                      </p>
+                    </div>
+                  </div> 
+                </div>
+              </div>`;
+        break;
+      }
+    case "huy":
+      {
+        text2 = 'huy ngon zai';
+        break;
+      }
+    case "nguyen": {
+      text2 = 'bao ngon zai';
+      break;
+    }
+    case "khoa": {
+      text2 = 'bao ngon zai';
+      break;
+    }
+    case "khanh": {
+      text2 = 'bao ngon zai';
+      break;
+    }
+    default: {
+      text2 = 'product 2';
+      break;
+    }
+  }
+  document.getElementById('pr2-compire').innerHTML = text2;
+});
